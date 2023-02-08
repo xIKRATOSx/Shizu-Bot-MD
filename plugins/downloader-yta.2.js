a159ea4c2d695253f1d40bdb3fc5302b7699f7c4
sandaru07 <108501284+sandaru07@users.noreply.github.com>
2023-02-08 20:49:37 +0530

    Update downloader-yta.2.js

--- plugins/downloader-yta.2.js
+++ plugins/downloader-yta.2.js
@@ -2,7 +2,7 @@ import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
-await m.reply(`*_⏳Downloading your song please wait...⏳_*\n\n*◉ For document type #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`)
+await m.reply(`*_⏳ please wait...⏳_*\n\n*◉ For document type #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`)
 try {
 let q = '128kbps'
 let v = args[0]
