b8e28c827096a6972dade968372d12366888e50c
Loother001 <122788306+Loother001@users.noreply.github.com>
2023-03-08 19:08:05 +0500

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -12,12 +12,12 @@ case "facebook": case "fb": case "facebookdl": case "fbdl":
 let res = await fbDownloader(args[0])
 for (let result of res.download) {
 let ur = result.url    
-await conn.sendFile(m.chat, ur, 'error.mp4', '*Made By BLack-KDX*', m)}
+await conn.sendFile(m.chat, ur, 'error.mp4', '*Made By NAWAB-XD*', m)}
 break           
 case "facebook2": case "fb2": case "facebookdl2": case "fbdl2":           
 let ress = await fg.fbdl(args[0])
 let urll = await ress.data[0].url    
-await conn.sendFile(m.chat, urll, 'error.mp4', '*Made By BLack-KDX*', m) 
+await conn.sendFile(m.chat, urll, 'error.mp4', '*Made By NAWAB-XD*', m) 
 break
 case "facebook3": case "fb3": case "facebookdl3": case "fbdl3":        
 let vio = await fetch(`https://api.violetics.pw/api/downloader/facebook?apikey=beta&url=${args[0]}`)  
@@ -27,13 +27,13 @@ await conn.sendFile(m.chat, videovio, `error.mp4`, '*Made By BLack-KDX*', m)
 break   
 case "facebook4": case "fb4": case "facebookdl4": case "fbdl4":           
 const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*Made By BLack-KDX*', m)
+for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*Made By NAWAB-XD*', m)
 break          
 case "facebook5": case "fb5": case "facebookdl5": case "fbdl5":        
 let res3 = await fetch(`https://latam-api.vercel.app/api/facebookdl?apikey=brunosobrino&q=${args[0]}`)  
 let json = await res3.json()
 let url3 = await json.video
-await conn.sendFile(m.chat, url3, 'error.mp4', '*Made By BLack-KDX*', m)     
+await conn.sendFile(m.chat, url3, 'error.mp4', '*Made By NAWAB-XD*', m)     
 break    
 }} catch {
 await await m.reply(`*[❗ERROR❗] ,When Uploading the video. Try in these.. (${usedPrefix}fb, ${usedPrefix}fb2, ${usedPrefix}fb3, ${usedPrefix}fb4, ${usedPrefix}fb5)*`)
